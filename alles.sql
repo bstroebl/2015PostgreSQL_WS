6c0f1e6ae4d04afdeeaa61cf29ac6222abed3271
Bernhard Ströbl <bernhard.stroebl@jena.de>
2019-04-03 07:17:29 +0200

    Füge Lizenzen hinzu

--- alles.sql
+++ alles.sql
@@ -1,3 +1,12 @@
+/* *************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ************************************************************************* */
+
 -- Hinweis: Öffnen Sie diese Datei im Editor medit
 -- Zunächst: Neue Datenbank anlegen, z.B. workshop
 -- Laden der PostGIS-Funktionen
